13a453bb8246be3022ec3ec4b996e6a49057e34b
laboratorioesan <laboratorioesan@esan.edu.pe>
2024-05-19 13:15:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yauri21100406ep202401/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.yauri21100406ep202401.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
